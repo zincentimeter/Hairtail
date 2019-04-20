9288003c2d81edeaff42e3ee40d71e6e83bc07fb
Mizuha <2240112342@qq.com>
2019-04-21 06:12:22 +0800

    Prepare for the connection of chain and webs (nearly not done).

--- web/main.js
+++ web/main.js
@@ -2,8 +2,9 @@ var picker = new Pikaday({
 	field: document.getElementById('datepicker'),
 	onSelect: function(date) {
 		website = document.getElementById("Website").value;
-		document.write("查询结果：在",date.toString(),"您查询的网站",website,"的访问量为：",23333);
+
+		document.write("在",date.toString(),"您查询的网站",website,"的访问量为：",23333);
+
+		getInfo(website, date.getTime());
 		}
 	});
-
-// import GScatterJS from 'gscatterjs-core';
